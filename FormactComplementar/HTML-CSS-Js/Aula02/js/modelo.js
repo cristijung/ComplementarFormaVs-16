331ae439e7f88e88401b57f5198659c595624588
CristinaJung <cristijung@gmail.com>
2025-09-24 15:34:02 -0300

    codes aula 02

---
+++ FormactComplementar/HTML-CSS-Js/Aula02/js/modelo.js
@@ -0,0 +1,15 @@
+//var
+function comVar() {
+    console.log(carro);
+    var carro = 'fusca';
+    console.log(carro);
+
+    if (true) {
+        var carro = 'kombi';
+        console.log(carro);
+    }
+
+    console.log(carro);
+}
+
+comVar();
